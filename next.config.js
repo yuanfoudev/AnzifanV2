2c694e73ef73757d4c8467326b310b6e8c77477f
元否 <41934143+yuanfoudev@users.noreply.github.com>
2024-05-22 13:49:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
         return config
     },
     images: {
-        domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'images.unsplash.com'],
+        domains: ['www.notion.so','notion.so','static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'images.unsplash.com'],
         remotePatterns: [
             {
                 protocol: 'https',
